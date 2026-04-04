e10ef33dd3e5237633748f6a3b72adf5e276ba09
justhallwest-commits <justhallwest@gmail.com>
2026-04-03 23:21:49 -0400

    Delete PluginServiceRegistrator.cs

--- PluginServiceRegistrator.cs
+++
@@ -1,4 +0,0 @@
-// IPluginServiceRegistrator was removed in Jellyfin 10.10+.
-// Jellyfin auto-discovers IIntroProvider implementations from the plugin assembly via DI.
-// The SessionInterceptor is started by PreRollIntroProvider's constructor,
-// which is itself auto-discovered and DI-instantiated by Jellyfin at startup.
